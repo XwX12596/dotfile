ec74ff7800428d3c7c5f064b17a7dd29a596c9a0
XwX12596 <1259685860@qq.com>
2023-11-21 14:13:15 +0800

    more new-scripts and edit old-scripts

--- scripts/qemu-clear.sh
+++ scripts/qemu-clear.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+umount /home/xwx/Media/VM/share*
 sudo tunctl -d tap0
 sudo tunctl -d tap1
 sudo iptables -t nat -D POSTROUTING -o enp0s31f6 -s 192.168.12.0/24 -j MASQUERADE
-umount $HOME/Windows/Media
--- scripts/qemu-win.sh
+++ scripts/qemu-win.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
-
 sudo tunctl -t tap1 -u xwx
 sudo ifconfig tap1 192.168.12.1 up
 sudo iptables -A FORWARD -i tap1 -o enp0s31f6 -j ACCEPT
 sudo iptables -A FORWARD -i enp0s31f6 -o tap1 -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -o enp0s31f6 -s 192.168.12.0/24 -j MASQUERADE
-mount $HOME/Windows/Media
-sudo qemu-system-x86_64 -m 8G \
-  -usb -device usb-tablet \
+qemu-system-x86_64 -m 8G \
+  -machine q35 -device intel-iommu,caching-mode=on \
+  -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time -smp 8 \
+  -device vfio-pci,host=01:00.0 \
+  -device virtio-vga-gl -display gtk,gl=on \
+  -device virtio-net,netdev=network1 \
+  -netdev tap,id=network1,ifname=tap1,script=no,downscript=no,vhost=on \
   -device qemu-xhci,id=xhci \
+  -usb -device usb-tablet \
   -device usb-host,bus=xhci.0,vendorid=0x1a86,productid=0x7523 \
-  -net nic,macaddr=52:54:00:12:34:67 \
-  -net tap,ifname=tap1,script=no,downscript=no \
+  -device usb-host,bus=xhci.0,vendorid=0x045e,productid=0x0b12 \
+  -device usb-host,bus=xhci.0,vendorid=0x0079,productid=0x181c \
+  -audiodev pipewire,id=snd0 \
+  -device ich9-intel-hda \
+  -device hda-output,audiodev=snd0 \
+  -drive if=pflash,format=raw,file=/home/xwx/Media/VM/OVMF.fd \
   --enable-kvm -boot order=c \
-  /home/xwx/Windows/Media/win10 &
+  /home/xwx/Media/VM/win10
+
+# remote-viewer spice://localhost:3001
+
+sh /usr/local/bin/qemu-clear.sh
+
+  #
+  # -vga qxl -device virtio-serial-pci \
+  # -spice port=3001,disable-ticketing=on \
+  # -device virtserialport,chardev=spice1,name=com.redhat.spice.1 \
+  # -chardev spicevmc,id=spice1,name=vdagent \
+  # -audiodev id=alsa,driver=alsa \
+  # -device usb-host,bus=xhci.0,vendorid=0x17ef,productid=0x6104 \
+  # -device usb-host,bus=xhci.0,vendorid=0x045e,productid=0x0b12 \
+  # -audiodev alsa,id=snd0 \
+  # -device ich9-intel-hda \
+  # -device hda-duplex,audiodev=snd0 \
+  #

e9cc7f3ae7e422bf09b27978f8cd712210175e08
xwx12596 <1259685860@qq.com>
2024-11-25 23:04:44 +0800

    Revert "wsl init"
    This reverts commit 82e42fe52dbf147f44c21fbb96f7897ca235aa6c.

--- update.sh
+++ update.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-if [[ -e ~/.config/mpv ]];then
-	rm -f mpv/
-	cp -r ~/.config/mpv/ ./
-else
-	echo NO MPV!
-fi
-if [[ -e ~/.config/nvim ]];then
-	rm -f nvim
-	cp -r ~/.config/nvim/ ./
-else
-	echo NO NEOVIM!
-fi
+rm -f ./config/*
+rm -rf mpv/ nvim/
 
 cp ~/.liverc config/
+cp ~/.xinitrc config/
+# cp ~/.Xmodmap config/
+cp ~/.Xresources config/
 cp ~/.zshrc config/
 cp ~/.config/yt-dlp/config ./config/yt-dlp.conf
 cp ~/.config/kitty/kitty.conf ./config/kitty.conf
@@ -20,7 +13,5 @@ cp ~/.config/kitty/Dracula.conf ./config/Dracula.conf
 
 cp -r ~/.config/Typora/themes/drake* ./typora/
 cp -r ~/Media/VM/install/ ./qemuinstall
-
-# cp ~/.xinitrc config/
-# cp ~/.Xmodmap config/
-# cp ~/.Xresources config/
+cp -r ~/.config/mpv/ ./
+cp -r ~/.config/nvim/ ./

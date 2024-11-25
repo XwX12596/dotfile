9ae355316e74e6e659c27af2008d3bdd31c8c63c
xwx12596 <1259685860@qq.com>
2024-11-25 23:11:10 +0800

    update.sh upd

--- update.sh
+++ update.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-rm -f ./config/*
-rm -rf mpv/ nvim/
+if [[ -e ~/.config/mpv ]];then
+	rm -f mpv/
+	cp -r ~/.config/mpv/ ./
+else
+	echo NO MPV!
+fi
+if [[ -e ~/.config/nvim ]];then
+	rm -f nvim
+	cp -r ~/.config/nvim/ ./
+else
+	echo NO NEOVIM!
+fi
 
 cp ~/.liverc config/
-cp ~/.xinitrc config/
-# cp ~/.Xmodmap config/
-cp ~/.Xresources config/
 cp ~/.zshrc config/
 cp ~/.config/yt-dlp/config ./config/yt-dlp.conf
 cp ~/.config/kitty/kitty.conf ./config/kitty.conf
@@ -13,5 +20,7 @@ cp ~/.config/kitty/Dracula.conf ./config/Dracula.conf
 
 cp -r ~/.config/Typora/themes/drake* ./typora/
 cp -r ~/Media/VM/install/ ./qemuinstall
-cp -r ~/.config/mpv/ ./
-cp -r ~/.config/nvim/ ./
+
+# cp ~/.xinitrc config/
+# cp ~/.Xmodmap config/
+# cp ~/.Xresources config/

3f70356033333a9f103369870327db32ced7790d
xwx12596 <1259685860@qq.com>
2025-04-18 15:43:48 +0800

    qemu command upd

--- scripts/qemu-arch.sh
+++ scripts/qemu-arch.sh
@@ -3,18 +3,18 @@
 VM_path='/home/xwx/Media/VM/'
 VM_img='arch'
 net_addr='192.168.14'
-sudo tunctl -t tap-install -u xwx
-sudo ifconfig tap-install ${net_addr}.1 up
-sudo iptables -A FORWARD -i tap-install -j ACCEPT
-sudo iptables -A FORWARD -o tap-install -j ACCEPT
+sudo ip tuntap add dev tap-${VM_img} mode tap
+sudo ifconfig tap-arch ${net_addr}.1 up
+sudo iptables -A FORWARD -i tap-${VM_img} -j ACCEPT
+sudo iptables -A FORWARD -o tap-${VM_img} -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -s ${net_addr}.0/24 -j MASQUERADE
 
 qemu-system-x86_64 -m 8G \
   -machine q35,smm=on \
   -cpu host -smp 16 \
   -drive if=pflash,format=raw,file=${VM_path}OVMF/OVMF-${VM_img}.fd \
-  -device virtio-net,netdev=network-install \
-  -netdev tap,id=network-install,ifname=tap-install,script=no,downscript=no,vhost=on \
+  -device virtio-net,netdev=network-${VM_img} \
+  -netdev tap,id=network-${VM_img},ifname=tap-${VM_img},script=no,downscript=no,vhost=on \
   -device qemu-xhci,id=xhci \
   -usb -device usb-tablet \
   -vga qxl -device virtio-serial-pci \
@@ -25,9 +25,10 @@ qemu-system-x86_64 -m 8G \
   ${VM_path}${VM_img} &
 
 remote-viewer spice://localhost:3001
-umount /home/xwx/Media/VM/share-lin
-sudo iptables -D FORWARD -i tap-install -j ACCEPT
-sudo iptables -D FORWARD -o tap-install -j ACCEPT
+# umount /home/xwx/Media/VM/share-lin
+sudo iptables -A FORWARD -i tap-${VM_img} -j ACCEPT
+sudo iptables -A FORWARD -o tap-${VM_img} -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -s ${net_addr}.0/24 -j MASQUERADE
 sudo iptables -t nat -D POSTROUTING -s ${net_addr}.0/24 -j MASQUERADE
-sudo tunctl -d tap-install
+sudo ip tuntap del dev tap-${VM_img} mode tap
 
--- scripts/qemu-clear.sh
+++ scripts/qemu-clear.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-umount /home/xwx/Media/VM/share*
-sudo iptables -D FORWARD -i tap0 -j ACCEPT
-sudo iptables -D FORWARD -o tap0 -j ACCEPT
-sudo iptables -D FORWARD -i tap1 -j ACCEPT
-sudo iptables -D FORWARD -o tap1 -j ACCEPT
+# umount /home/xwx/Media/VM/share*
+sudo iptables -D FORWARD -i tap-${1} -j ACCEPT
+sudo iptables -D FORWARD -o tap-${1} -j ACCEPT
 sudo iptables -t nat -D POSTROUTING -s 192.168.12.0/24 -j MASQUERADE
-sudo tunctl -d tap0
-sudo tunctl -d tap1
+sudo ip tuntap del dev tap-install mod tap

31b3d81c7f21ec64040a77a2a5bb782040b3e059
XwX12596 <1259685860@qq.com>
2026-01-03 16:40:49 +0800

    rdp

--- scripts/rdp.sh
+++ scripts/rdp.sh
@@ -1,1 +1,1 @@
-rdesktop -g 2560x1440@122 -P -z -x l -r sound:remote -u xwx -p Hh2001 -r clipboard:PRIMARYCLIPBOARD ${1}:3389
+rdesktop -g 2560x1440@164 -P -z -x l -r sound:remote -u xwx -p Hh2001 -r clipboard:PRIMARYCLIPBOARD ${1}:3389

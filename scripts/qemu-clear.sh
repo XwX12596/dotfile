22797c3641f793bb311798842cf02f7f5b588446
xwx12596 <1259685860@qq.com>
2026-01-18 23:26:43 +0800

    delete all dwm scripts

--- scripts/qemu-clear.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo umount /home/xwx/Media/VM/share-${1}
-sudo iptables -D FORWARD -i tap-${1} -j ACCEPT
-sudo iptables -D FORWARD -o tap-${1} -j ACCEPT
-sudo iptables -t nat -D POSTROUTING -s 192.168.12.0/24 -j MASQUERADE
-sudo ip tuntap del dev tap-${1} mod tap
--- scripts/qemu-net.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-net_addr='192.168.14'
-sudo ip tuntap add dev tap-arch mod tap
-sudo ifconfig tap-arch ${net_addr}.1 up
-sudo iptables -A FORWARD -i tap-install -j ACCEPT
-sudo iptables -A FORWARD -o tap-install -j ACCEPT
-sudo iptables -t nat -A POSTROUTING -s ${net_addr}.0/24 -j MASQUERADE
